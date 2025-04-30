deabfdb15d50b0e866d14ea21ca064432684cee3
Aaron Echler <acechler@gmail.com>
2025-04-30 16:40:08 -0400

    Create an entity object that inherits square.

--- love-workspace/main.lua
+++ love-workspace/main.lua
@@ -1,14 +1,13 @@
 -- How to Run
 -- 1. Make sure terminal has love-workspace folder open
--- type "love ." into the terminal
+-- 2. Type "love ." into the terminal
 
--- main.lua
 local Square = require("square")
 
 local square
 
 function love.load()
-    square = Square.new(50, 50)
+    square = Square:new(50, 50) -- use colon syntax for class method
     square.pathfinder:addTarget(200, 100)
     square.pathfinder:addTarget(300, 300)
     square.pathfinder:addTarget(100, 300)
@@ -16,6 +15,7 @@ end
 
 function love.update(dt)
     square:update(dt)
+
     -- Click to dynamically add new waypoints
     if love.mouse.isDown(1) then
         local mx, my = love.mouse.getPosition()
--- love-workspace/square.lua
+++ love-workspace/square.lua
@@ -1,15 +1,17 @@
--- square.lua
+local Entity = require("Entity")
 local Pathfinder = require("pathfinder")
 
-local Square = {}
+-- Define Square first as a subclass of Entity
+local Square = setmetatable({}, { __index = Entity })
 Square.__index = Square
 
-function Square.new(x, y)
-    local self = setmetatable({}, Square)
-    self.x = x
-    self.y = y
+-- Define constructor using colon syntax (preferred for inheritance)
+function Square:new(x, y)
+    local self = Entity.new(self, x, y)
+    setmetatable(self, Square) -- important: set metatable after Entity.new
     self.size = 20
-    self.pathfinder = Pathfinder.new(self, 100) -- Attach a Pathfinder component
+    self.pathfinder = Pathfinder.new(self, 100)
+    self.color = {0, 1, 0, 1}
     return self
 end
 
@@ -18,7 +20,7 @@ function Square:update(dt)
 end
 
 function Square:draw()
-    love.graphics.setColor(0, 1, 0)
+    love.graphics.setColor(self.color)
     love.graphics.rectangle("fill", self.x - self.size/2, self.y - self.size/2, self.size, self.size)
     self.pathfinder:drawDebug()
 end
