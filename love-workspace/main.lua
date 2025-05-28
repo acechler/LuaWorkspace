596b8568d94a36c76c2c93c0440699f9b726f409
Aaron Echler <acechler@gmail.com>
2025-05-28 15:35:17 -0400

    Clean out the love directory now that the code is stored in old-love.md

--- love-workspace/main.lua
+++ love-workspace/main.lua
@@ -2,59 +2,25 @@
 -- 1. Make sure terminal has love-workspace folder open
 -- 2. Type "love ." into the terminal
 
-local Graph = require("graph")
-local Square = require("square")
-
-local squareGraph
-local squareRefs = {} -- optional: to keep track of squares by ID
-
 function love.load()
-    squareGraph = Graph:new(false) -- false = undirected
-
-    -- Create Squares and add to graph
-    for i = 1, 5 do
-        local sq = Square:new(100 + i * 60, 200)
-        local id = "Square" .. i
-        squareRefs[id] = sq
-        squareGraph:addNode(id)
-    end
-
-    -- Connect squares in a line: Square1 <-> Square2 <-> ... <-> Square5
-    for i = 1, 4 do
-        local from = "Square" .. i
-        local to = "Square" .. (i + 1)
-        squareGraph:addEdge(from, to)
-    end
+    x, y = 100, 100
+    speed = 200
 end
 
 function love.update(dt)
-    for id, square in pairs(squareRefs) do
-        square:update(dt)
+    if love.keyboard.isDown("right") then
+        x = x + speed * dt
+    elseif love.keyboard.isDown("left") then
+        x = x - speed * dt
+    end
+
+    if love.keyboard.isDown("down") then
+        y = y + speed * dt
+    elseif love.keyboard.isDown("up") then
+        y = y - speed * dt
     end
 end
 
 function love.draw()
-    -- Draw squares
-    for id, square in pairs(squareRefs) do
-        square:draw()
-    end
-
-    -- Draw connections
-    love.graphics.setColor(1, 1, 0)
-    squareGraph:forEachEdge(function(from, to)
-        local a = squareRefs[from]
-        local b = squareRefs[to]
-        if a and b then
-            love.graphics.line(a.x, a.y, b.x, b.y)
-        end
-    end)
-end
-
--- Click to assign a new target to all Squares
-function love.mousepressed(x, y, button)
-    if button == 1 then
-        for _, square in pairs(squareRefs) do
-            square.pathfinder:addTarget(x, y)
-        end
-    end
+    love.graphics.circle("fill", x, y, 20)
 end
